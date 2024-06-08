edfe45559cc84a0e482f326be1d59100d6459341
Van-Excel <vanexcel.investments@gmail.com>
2024-06-08 20:22:57 +0000

    using views

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ var express = require("express");
 const path = require("path")
 
 
-app = express();
+
+var app = express();
 
 // configuring express to serve static files
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
+//configure views and template engine
+// views are in a folder called views
+app.set("views", path.resolve(__dirname, "views"));
+
+// set template engine
+app.set("view engine", "ejs");
+
+// MIDDLEWARE
+
 app.get("/", function(req, res){
-    res.end("Welcome to the homepage.");
+    res.render("index", {"message": "Hello this is the homepage"});
 });
 
 app.get("/about", function(req, res){
