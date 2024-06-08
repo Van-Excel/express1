000470e9e393f80029cc5d4bd00e9db733f77863
Van-Excel <vanexcel.investments@gmail.com>
2024-06-08 19:01:59 +0000

    added routing

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var http = require("node:http");
-var express = require("node:express")
+var express = require("express");
 
 
-app = express()
+app = express();
+
+// configuring express to serve static files
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", function(req, res){
+    res.end("Welcome to the homepage.");
+});
+
+app.get("/about", function(req, res){
+    res.end("Welcome to the about page.");
+});
 
 
-http.createServer(app).listen(3000)
+app.use(function(req, res){
+    res.statuscode = 404;
+    res.end("Page not found");
+});
+
+
+http.createServer(app).listen(3000);
